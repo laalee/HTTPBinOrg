8418de924e459a3482c96d22dbc2f83dd304b86d
annieli <annieli@kkbox.com>
2019-01-30 18:49:35 +0800

    Create new project.

---
+++ HTTPBinOrg/HTTPBinOrg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPBinOrg
+//
+//  Created by Annie Lee on 2019/1/30.
+//  Copyright © 2019 Annie Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPBinOrg/HTTPBinOrg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPBinOrg
+//
+//  Created by Annie Lee on 2019/1/30.
+//  Copyright © 2019 Annie Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
